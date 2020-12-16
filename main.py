7a7ac9548cab04c9042fa4091886aee3ada1ee15
shine_speed <>
2020-12-16 22:55:53 +0200

    one

---
+++ main.py
@@ -0,0 +1,19 @@
+class Car:
+    __model = 'undefined'
+    __speed = 0
+
+    def __init__(self, speed, model):
+        self.__model = model
+        self.__speed = speed
+
+    def print_character(self):
+        print(self.__model, self.__speed)
+
+
+class ElectricalCar(Car):
+    def __init__(self):
+        super().__init__(self)
+
+
+car = Car(250, "lada")
+"""car.print_character()"""
